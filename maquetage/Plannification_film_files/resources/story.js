bab472e37b626976dc243ba7ddda147b5e5b07dc
Jérémy-Duval <jeremy-duval1@hotmail.fr>
2016-12-05 15:27:47 +0100

    Fin maquetage plannification film

---
+++ maquetage/Plannification_film_files/resources/story.js
@@ -0,0 +1,447 @@
+var story = {
+ "pages": [
+  {
+   "image": "calendrier.png",
+   "width": 1397,
+   "links": [
+    {
+     "rect": [
+      91,
+      76,
+      192,
+      103
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      192,
+      76,
+      299,
+      103
+     ],
+     "page": 5
+    }
+   ],
+   "title": "calendrier",
+   "height": 745
+  },
+  {
+   "image": "ajouterFilm.png",
+   "width": 1318,
+   "links": [
+    {
+     "rect": [
+      16,
+      76,
+      91,
+      103
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      192,
+      76,
+      299,
+      103
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      215,
+      240,
+      265,
+      263
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      135,
+      688,
+      197,
+      713
+     ],
+     "page": 0
+    }
+   ],
+   "title": "ajouterFilm",
+   "height": 745
+  },
+  {
+   "image": "ajouterFilm2.png",
+   "width": 1318,
+   "links": [
+    {
+     "rect": [
+      16,
+      76,
+      91,
+      103
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      91,
+      76,
+      192,
+      103
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      192,
+      76,
+      299,
+      103
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      300,
+      400,
+      351,
+      423
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      447,
+      576,
+      540,
+      599
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      447,
+      488,
+      540,
+      511
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      287,
+      313,
+      365,
+      336
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      135,
+      688,
+      197,
+      713
+     ],
+     "page": 1
+    }
+   ],
+   "title": "ajouterFilm2",
+   "height": 745
+  },
+  {
+   "image": "ajouterFilm3.png",
+   "width": 1318,
+   "links": [
+    {
+     "rect": [
+      16,
+      76,
+      91,
+      103
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      91,
+      76,
+      192,
+      103
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      192,
+      76,
+      299,
+      103
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      31,
+      688,
+      86,
+      713
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      135,
+      688,
+      197,
+      713
+     ],
+     "page": 1
+    }
+   ],
+   "title": "ajouterFilm3",
+   "height": 745
+  },
+  {
+   "image": "calendrier(1).png",
+   "width": 1397,
+   "links": [
+    {
+     "rect": [
+      91,
+      76,
+      192,
+      103
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      192,
+      76,
+      299,
+      103
+     ],
+     "page": 5
+    }
+   ],
+   "title": "calendrier(1)",
+   "height": 745
+  },
+  {
+   "image": "modifierFilm.png",
+   "width": 1318,
+   "links": [
+    {
+     "rect": [
+      16,
+      76,
+      91,
+      103
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      91,
+      76,
+      192,
+      103
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      215,
+      192,
+      265,
+      215
+     ],
+     "page": 6
+    },
+    {
+     "rect": [
+      135,
+      688,
+      197,
+      713
+     ],
+     "page": 0
+    }
+   ],
+   "title": "modifierFilm",
+   "height": 745
+  },
+  {
+   "image": "modifierFilm2.png",
+   "width": 1318,
+   "links": [
+    {
+     "rect": [
+      0,
+      0,
+      1312,
+      745
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      16,
+      76,
+      91,
+      103
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      91,
+      76,
+      192,
+      103
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      302,
+      424,
+      353,
+      447
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      449,
+      600,
+      542,
+      623
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      449,
+      512,
+      542,
+      535
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      215,
+      192,
+      288,
+      215
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      289,
+      337,
+      367,
+      360
+     ],
+     "page": 7
+    },
+    {
+     "rect": [
+      135,
+      688,
+      197,
+      713
+     ],
+     "page": 5
+    }
+   ],
+   "title": "modifierFilm2",
+   "height": 745
+  },
+  {
+   "image": "modifierFilm3.png",
+   "width": 1318,
+   "links": [
+    {
+     "rect": [
+      16,
+      76,
+      91,
+      103
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      91,
+      76,
+      192,
+      103
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      192,
+      76,
+      299,
+      103
+     ],
+     "page": 5
+    },
+    {
+     "rect": [
+      31,
+      688,
+      86,
+      713
+     ],
+     "page": 0
+    },
+    {
+     "rect": [
+      135,
+      688,
+      197,
+      713
+     ],
+     "page": 5
+    }
+   ],
+   "title": "modifierFilm3",
+   "height": 745
+  },
+  {
+   "image": "calendrier(1)(1).png",
+   "width": 1397,
+   "links": [
+    {
+     "rect": [
+      91,
+      76,
+      192,
+      103
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      192,
+      76,
+      299,
+      103
+     ],
+     "page": 5
+    }
+   ],
+   "title": "calendrier(1)(1)",
+   "height": 745
+  }
+ ],
+ "title": "Plannification_film",
+ "highlightLinks": false
+}
