e1ffcf8a0bda219df771d5a962047e186fede41d
Jérémy-Duval <jeremy-duval1@hotmail.fr>
2016-12-12 14:26:30 +0100

    Base de l'appli Java de plannification des films Base de la Javadoc

---
+++ PlannificationFilm/src/plannificationfilm/controleur/PlannificationFilm.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plannificationfilm.controleur;
+
+/**
+ * Classe controleur, classe principale.
+ * @author Jérémy
+ * @since 12/12/2016
+ */
+public class PlannificationFilm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
