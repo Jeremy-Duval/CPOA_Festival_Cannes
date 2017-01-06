4f781f0eb82567e51df4b7db9696cc6d5be0283d
Jérémy-Duval <jeremy-duval1@hotmail.fr>
2017-01-06 10:36:47 +0100

    Fusion finale... Ouf ! ^^

---
+++ PlannificationFilm/src/plannificationfilm/vue/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Package contenant les IHM.
+ * @author Jérémy
+ * @since 12/12/2016
+ */
+package plannificationfilm.vue;
